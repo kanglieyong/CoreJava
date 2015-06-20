dfe1e08f99aa87c70005a0ec0340483f62101b61
kanglieyong <kanglieyong@gmail.com>
2015-06-21 00:29:14 +0800

    anonymousInnerClass

---
+++ bases/anonymousInnerClass/AnonymousInnerClassTest.java
@@ -0,0 +1,37 @@
+package anonymousInnerClass;
+
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class AnonymousInnerClassTest
+{
+    public static void main(String[] args)
+    {
+	TalkingClock clock = new TalkingClock();
+	clock.start(1000, true);
+
+	JOptionPane.showMessageDialog(null, "Quit program?");
+	System.exit(0);
+    }
+}
+
+class TalkingClock
+{
+    public void start(int interval, final boolean beep)
+    {
+	ActionListener listener = new ActionListener()
+	    {
+		public void actionPerformed(ActionEvent event)
+		{
+		    Date now = new Date();
+		    System.out.println("At the tone, the time is " + now);
+		    if (beep) Toolkit.getDefaultToolkit().beep();
+		}
+	    };
+	Timer t = new Timer(interval, listener);
+	t.start();
+    }
+}
