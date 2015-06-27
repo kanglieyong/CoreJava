94cbd8fe6dc9c43d78625fb686f353a3cc4aac19
kanglieyong <kanglieyong@gmail.com>
2015-06-28 04:22:32 +0800

    ActionListener

---
+++ bases/button/ButtonFrameTest.java
@@ -0,0 +1,21 @@
+package button;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ButtonFrameTest
+{
+    public static void main(String[] args)
+    {
+	EventQueue.invokeLater(new Runnable() {
+		public void run()
+		{
+		    JFrame frame = new ButtonFrame();
+		    frame.setTitle("ButtonFrame");
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    frame.setVisible(true);
+		}
+	    });
+    }
+}
