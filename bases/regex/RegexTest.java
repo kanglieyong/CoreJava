cd40c161fb73cafd6b49ea4094c06d911a7ab87f
kanglieyong <kanglieyong@gmail.com>
2015-08-05 19:09:18 +0800

    regex

---
+++ bases/regex/RegexTest.java
@@ -0,0 +1,50 @@
+package regex;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class RegexTest
+{
+    public static void main(String[] args) throws PatternSyntaxException
+    {
+        Scanner in = new Scanner(System.in);
+	System.out.println("Enter pattern: ");
+	String patternStr = in.nextLine();
+
+	Pattern pattern = Pattern.compile(patternStr);
+
+	while (true) {
+	    System.out.println("Enter string to match: ");
+	    String input = in.nextLine();
+	    if (input == null || input.equals("")) return;
+	    Matcher matcher = pattern.matcher(input);
+	    if (matcher.matches()) {
+	        System.out.println("Match");
+		int g = matcher.groupCount();
+		if (g > 0) {
+		    for (int i = 0; i < input.length(); i++) {
+		        for (int j = 1; j <= g; j++) {
+			    if (i == matcher.start(j) && i == matcher.end(j)) {
+			        System.out.print("()");
+			    }
+			}
+			for (int j = 1;j <= g; j++) {
+			    if (i == matcher.start(j) && i != matcher.end(j)) {
+			        System.out.print("(");
+			    }
+			}
+			System.out.print(input.charAt(i));
+			for (int j = 1; j <= g; j++) {
+			    if (i + 1 != matcher.start(j) && i + 1 == matcher.end(j)) {
+			        System.out.print(")");
+			    }
+			}
+		    }
+		    System.out.println();
+		}
+	    } else {
+	        System.out.println("No match");
+	    }
+	}
+    }
+}
